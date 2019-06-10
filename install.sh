167417a2e94e9eb557d652b145a57b9d727d7e61
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-06-11 01:00:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,38 +45,38 @@ echo "================= Installing Git $GIT_VERSION ===================="
 sudo yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 sudo yum install -y git-"$GIT_VERSION"
 
-export GCLOUD_SDKREPO=249.0*
-echo "================= Adding gcloud $GCLOUD_SDKREPO  ============"
-sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
-#adding key required to install gcloud
-rpm --import  https://packages.cloud.google.com/yum/doc/yum-key.gpg
-rpm --import  https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-sudo yum install -y google-cloud-sdk-"$GCLOUD_SDKREPO"
+# export GCLOUD_SDKREPO=249.0*
+# echo "================= Adding gcloud $GCLOUD_SDKREPO  ============"
+# sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+# [google-cloud-sdk]
+# name=Google Cloud SDK
+# baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+# enabled=1
+# gpgcheck=1
+# repo_gpgcheck=1
+# gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+#        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# EOM
+# #adding key required to install gcloud
+# rpm --import  https://packages.cloud.google.com/yum/doc/yum-key.gpg
+# rpm --import  https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# sudo yum install -y google-cloud-sdk-"$GCLOUD_SDKREPO"
 
-export AWS_VERSION=1.16.173
-echo "================= Adding awscli $AWS_VERSION ===================="
-sudo pip install awscli=="$AWS_VERSION"
+# export AWS_VERSION=1.16.173
+# echo "================= Adding awscli $AWS_VERSION ===================="
+# sudo pip install awscli=="$AWS_VERSION"
 
-AZURE_CLI_VERSION=2.0*
-echo "================ Adding azure-cli $AZURE_CLI_VERSION  =============="
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
-sudo yum install -y azure-cli-$AZURE_CLI_VERSION
+# AZURE_CLI_VERSION=2.0*
+# echo "================ Adding azure-cli $AZURE_CLI_VERSION  =============="
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+# sudo yum install -y azure-cli-$AZURE_CLI_VERSION
 
-export JFROG_VERSION=1.25.0
-echo "================= Adding jfrog-cli $JFROG_VERSION==================="
-wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
-sudo chmod +x jfrog
-sudo mv jfrog /usr/bin/jfrog
+# export JFROG_VERSION=1.25.0
+# echo "================= Adding jfrog-cli $JFROG_VERSION==================="
+# wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
+# sudo chmod +x jfrog
+# sudo mv jfrog /usr/bin/jfrog
 
 echo "================== Installing java packages ==================="
 
