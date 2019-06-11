cdf0dd76031a9f38197152d0f1b0240ee2686a98
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-06-11 12:28:48 +0530

    Update openjdk11.sh

--- version/openjdk11.sh
+++ version/openjdk11.sh
@@ -11,8 +11,3 @@ mv jdk-"$OPENJDK_VERSION"/ java-"$JAVA_VERSION"-openjdk-amd64
 
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/java 1
 sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/javac 1
-sudo update-alternatives --set java /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/javac
-
-echo "export JAVA_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64" >> /etc/drydock/.env
-echo "export PATH=$PATH:/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64/bin/java/bin" >> /etc/drydock/.env
